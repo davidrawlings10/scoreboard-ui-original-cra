0e560b5cb8bbce7cd64434cbf31a7d781af5eec0
David Rawlings <davidrawlings10@gmail.com>
2021-06-17 17:54:55 -0700

    test for TeamDisplay Cache

---
+++ src/TeamDisplayCache.test.js
@@ -0,0 +1,13 @@
+import { searchCacheForTeam, cacheTeam } from "./TeamDisplayCache";
+
+test("cache and then search for team", () => {
+  const teamLions = { id: 1, name: "Lions" };
+  const teamBears = { id: 2, name: "Bears" };
+  const teamTigers = { id: 3, name: "Tigers" };
+  cacheTeam(teamLions.id, teamLions);
+  cacheTeam(teamBears.id, teamBears);
+  expect(searchCacheForTeam(1).name).toBe("Lions");
+  cacheTeam(teamTigers.id, teamTigers);
+  expect(searchCacheForTeam(3).name).toBe("Tigers");
+  expect(searchCacheForTeam(2).name).toBe("Bears");
+});
