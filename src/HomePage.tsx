db992a8d6953dad17efc9e39903b52153c5f8fb8
David Rawlings <davidrawlings10@gmail.com>
2021-07-02 22:34:34 -0700

    converting HomePage to typescript function component and other clean up

---
+++ src/HomePage.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Scoreboard from "./Scoreboard";
+import Season from "./Season";
+import CurrentGameList from "./CurrentGameList";
+import Button from "./Components/Button";
+import PlayPauseToggle from "./PlayPauseToggle";
+import Game from "./Entity/Game";
+
+export type HomeProps = {
+  playPauseToggle: Boolean;
+  currentGames: Array<Game>;
+  displayGameIndex: number;
+  seasonId: number;
+};
+
+export default function App(props: HomeProps) {
+  const [playPauseToggle, setPlayPauseToggle] = React.useState(true);
+  const [currentGames, setCurrentGames] = React.useState(Array<Game>());
+  const [displayGameIndex, setDisplayGameIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    if (playPauseToggle) {
+      setGetGamesInterval();
+    }
+    return function cleanup() {
+      clearGetGamesInterval();
+    };
+  });
+
+  let timerId: any;
+
+  function setGetGamesInterval() {
+    timerId = setInterval(() => getGames(), 500);
+  }
+
+  function clearGetGamesInterval() {
+    clearInterval(timerId);
+  }
+
+  function getGames() {
+    fetch("http://localhost:8080/game/getGames")
+      .then((res) => res.json())
+      .then((json) => {
+        setCurrentGames(json.list);
+      });
+  }
+
+  function onToggleChange() {
+    if (!playPauseToggle) {
+      fetch("http://localhost:8080/game/playGames");
+      setGetGamesInterval();
+    } else {
+      fetch("http://localhost:8080/game/pauseGames");
+      clearGetGamesInterval();
+    }
+    setPlayPauseToggle(!playPauseToggle);
+  }
+
+  return (
+    <div className="Home">
+      <CurrentGameList
+        games={
+          currentGames != null && currentGames.length > 0 ? currentGames : null
+        }
+        updateDisplayIndex={(index) => setDisplayGameIndex(index)}
+      />
+      <Button onChange={onToggleChange} style={{ display: "none" }}>
+        {playPauseToggle ? "ON" : "OFF"}
+      </Button>
+      <PlayPauseToggle
+        toggleValue={playPauseToggle}
+        onChange={onToggleChange}
+      />
+      <Scoreboard
+        game={
+          currentGames != null && currentGames.length > 0
+            ? currentGames[displayGameIndex]
+            : null
+        }
+      />
+      <Season seasonId={currentGames[displayGameIndex]?.seasonId} />
+      <div style={{ marginTop: 100 }}></div>
+    </div>
+  );
+}
