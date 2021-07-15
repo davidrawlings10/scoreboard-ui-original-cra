6966af65098755ef1c3ce5da08d09bcb1d727862
David Rawlings <davidrawlings10@gmail.com>
2021-07-14 19:02:29 -0700

    adding styled-components

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,29 @@
+// const styled = require("styled-components");
+import styled from "styled-components";
+
 export type HeaderProps = {};
+
+const Span = styled.span`
+  margin: 10px;
+`;
+
+const Div = styled.div`
+  margin: 10px 0px 10px 0px;
+`;
 
 const Header = (props: HeaderProps) => {
   return (
-    <div className="Header">
-      <span style={{ padding: 10 }}>
+    <Div>
+      <Span>
         <a href="http://localhost:3000">Home</a>
-      </span>
-      <span style={{ padding: 10 }}>
+      </Span>
+      <Span>
         <a href="http://localhost:3000/season">Season</a>
-      </span>
-      <span style={{ padding: 10 }}>
+      </Span>
+      <Span>
         <a href="http://localhost:3000/addGame">Add Game</a>
-      </span>
-    </div>
+      </Span>
+    </Div>
   );
 };
 
