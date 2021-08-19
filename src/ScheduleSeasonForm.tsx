2dae0d1bbe7b8af6afb346195b5afac0ed2d2465
David Rawlings <davidrawlings10@gmail.com>
2021-08-19 16:49:52 -0700

    displaying list of checkboxes for setting which teams to include in a season

--- src/ScheduleSeasonForm.tsx
+++ src/ScheduleSeasonForm.tsx
@@ -4,8 +4,12 @@ import {
   MenuItem,
   Input,
   TextField,
+  FormGroup,
+  Checkbox,
+  FormControlLabel,
 } from "@material-ui/core";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Team from "./Entity/Team";
 
 export type ScheduleSeasonFormProps = {};
 
@@ -15,6 +19,8 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
   const [sport, setSport] = useState<string>("HOCKEY");
   const [leagueId, setLeagueId] = useState<number>(2);
   const [numGames, setNumGames] = useState<number>(4);
+  const [possibleTeams, setPossibleTeams] = useState<Array<Team>>([]);
+  const [selectedTeamIds, setSelectedTeamIds] = useState<Array<number>>([]);
 
   function titleChange(event: React.ChangeEvent<any>) {
     setTitle(event.target.value);
@@ -36,6 +42,31 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
     setNumGames(event.target.value);
   }
 
+  function selectedTeamIdsChange(event: React.ChangeEvent<HTMLInputElement>) {
+    /*console.log("event.target.checked:   " + event.target.checked);
+    console.log("event.target.value:     " + event.target.value);
+    console.log("selectedTeamIds before: " + selectedTeamIds);*/
+
+    // must perform push and splice on deep copy because it can't be performed on state array directly
+    // let selectedTeamIdsCopy: Array<number> = selectedTeamIds.map((id) => id);
+    if (event.target.checked) {
+      // selectedTeamIdsCopy.push(parseInt(event.target.value));
+      setSelectedTeamIds(selectedTeamIds.concat(parseInt(event.target.value)));
+    } else {
+      /*selectedTeamIdsCopy.splice(
+        selectedTeamIds.indexOf(parseInt(event.target.value)),
+        1
+      );*/
+      setSelectedTeamIds(
+        selectedTeamIds.filter((id) => id !== parseInt(event.target.value))
+      );
+    }
+    // setSelectedTeamIds(selectedTeamIdsCopy);
+
+    /*console.log("selectedTeamIds after:  " + selectedTeamIds);
+    console.log("selectedTeamIdsCopy after:  " + selectedTeamIdsCopy);*/
+  }
+
   function handleSubmit(event: React.ChangeEvent<any>) {
     fetch(
       "http://localhost:8080/season/schedule?scheduleType=" +
@@ -44,6 +75,8 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
         sport +
         "&leagueId=" +
         leagueId +
+        "&teamIds=" +
+        selectedTeamIds +
         "&numGames=" +
         numGames +
         "&title=" +
@@ -51,6 +84,15 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
     );
     event.preventDefault();
   }
+
+  useEffect(() => {
+    fetch("http://localhost:8080/team/getTeams?leagueId=" + leagueId)
+      .then((res) => res.json())
+      .then((json) => {
+        setPossibleTeams(json.list);
+        setSelectedTeamIds(json.list.map((team: Team) => team.id));
+      });
+  }, [leagueId]);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -72,6 +114,20 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
       <InputLabel>Number of Games</InputLabel>
       <TextField value={numGames} onChange={numGamesChange} />
       <Input type="submit" value="Submit" />
+      <FormGroup>
+        {possibleTeams.map((team) => (
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={selectedTeamIds.indexOf(team.id) >= 0}
+                onChange={selectedTeamIdsChange}
+                value={team.id}
+              />
+            }
+            label={team.location + " " + team.name}
+          />
+        ))}
+      </FormGroup>
     </form>
   );
 }
