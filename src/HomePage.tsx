c4a1dc62cee71f03d6258e1c7b60ac483bc0de7c
David Rawlings <davidrawlings10@gmail.com>
2021-10-06 14:12:04 -0700

    adding dialog for game state controls

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -4,7 +4,19 @@ import SeasonDisplay from "./SeasonDisplay";
 import CurrentGameList from "./CurrentGameList";
 import Game from "./Entity/Game";
 import TickMilliInput from "./TickMilliInput";
-import { Switch, Box, FormControlLabel } from "@material-ui/core";
+import {
+  Switch,
+  Box,
+  FormControlLabel,
+  TextField,
+  Button,
+} from "@material-ui/core";
+
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default function App() {
   const [currentGames, setCurrentGames] = React.useState(Array<Game>());
@@ -17,8 +29,8 @@ export default function App() {
     React.useState<boolean>(false);
 
   const [gamesToPlay, setGamesToPlay] = React.useState<number>(0);
-  const [gamesToPlayTyping, setGamesToPlayTyping] =
-    React.useState<boolean>(false);
+  /*const [gamesToPlayTyping, setGamesToPlayTyping] =
+    React.useState<boolean>(false);*/
 
   let timerId: any = null;
 
@@ -28,18 +40,18 @@ export default function App() {
       .then((json) => {
         setCurrentGames(json.games);
         setRunning(json.running);
-        console.log(
+        /*console.log(
           "tickMilliUserTyping:" +
             tickMilliUserTyping +
             ", gamesToPlayTyping:" +
             gamesToPlayTyping
-        );
+        );*/
         if (!tickMilliUserTyping) {
           setTickMilli(json.tickMilliseconds);
         }
-        if (!gamesToPlayTyping) {
-          setGamesToPlay(json.gamesToPlay);
-        }
+        /*if (!gamesToPlayTyping) {*/
+        setGamesToPlay(json.gamesToPlay);
+        /*}*/
       });
   }
 
@@ -100,7 +112,7 @@ export default function App() {
     setRunning(event.target.checked);
   };
 
-  const handleGamesToPlayBlur = () => {
+  /*const handleGamesToPlayBlur = () => {
     fetch("http://localhost:8080/game/setGamesToPlay?numGames=" + gamesToPlay);
     console.log(
       "handleGamesToPlayBlur() BEFORE - gamesToPlayTyping:" + gamesToPlayTyping
@@ -123,6 +135,32 @@ export default function App() {
     console.log(
       "handleGamesToPlayChange() AFTER - gamesToPlayTyping:" + gamesToPlayTyping
     );
+  };*/
+
+  const [open, setOpen] = React.useState(false);
+  const [gamesToPlayInput, setGamesToPlayInput] = React.useState<number>(0);
+
+  const handleClickOpen = () => {
+    setGamesToPlayInput(gamesToPlay);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    setGamesToPlay(gamesToPlayInput);
+    fetch(
+      "http://localhost:8080/game/setGamesToPlay?numGames=" + gamesToPlayInput
+    );
+    setOpen(false);
+  };
+
+  const gamesToPlayInputOnChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setGamesToPlayInput(parseInt(event.target.value));
   };
 
   return (
@@ -144,15 +182,58 @@ export default function App() {
           <FormControlLabel
             label="Number of Games to Play"
             control={
-              <input
-                onChange={handleGamesToPlayChange}
-                onBlur={handleGamesToPlayBlur}
-                value={gamesToPlay}
-              />
+              <>
+                <input
+                  /*onChange={handleGamesToPlayChange}
+                  onBlur={handleGamesToPlayBlur}*/
+                  value={gamesToPlay}
+                  readOnly={true}
+                />
+              </>
             }
             labelPlacement="start"
           />
+          <Button onClick={handleClickOpen} color="primary">
+            Edit
+          </Button>
         </Box>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Edit Games to Play</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              id="tickMilliseconds"
+              label="Tick Milliseconds"
+              type="number"
+              fullWidth
+            />
+            <TextField
+              autoFocus
+              id="gamesToPlay"
+              label="Games to Play"
+              type="number"
+              value={gamesToPlayInput}
+              fullWidth
+              onChange={gamesToPlayInputOnChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleSubmit} color="primary">
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
         <CurrentGameList
           games={
             currentGames != null && currentGames.length > 0
