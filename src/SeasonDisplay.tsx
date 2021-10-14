0650845e52c6f0ae8220098f9ee48ded0fe6d041
David Rawlings <davidrawlings10@gmail.com>
2021-10-14 13:45:58 -0700

    add snackbar when game is started

--- src/SeasonDisplay.tsx
+++ src/SeasonDisplay.tsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Snackbar } from "@material-ui/core";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+
 import SeasonStanding from "./SeasonStanding";
 import SeasonGameList from "./SeasonGameList";
 import SeasonStandingSortable from "./SeasonStandingSortable";
@@ -9,9 +13,23 @@ export type SeasonProps = {
   seasonId: number;
 };
 
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 export default function SeasonDisplay(props: SeasonProps) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   function playSeasonGame() {
-    console.log("startSeasonGame()");
+    setOpen(true);
     fetch(
       "http://localhost:8080/game/startSeasonGame?seasonId=" + props.seasonId
     );
@@ -23,7 +41,13 @@ export default function SeasonDisplay(props: SeasonProps) {
 
   return (
     <div>
+      <Snackbar open={open} autoHideDuration={1000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          Game started
+        </Alert>
+      </Snackbar>
       <Button onClick={playSeasonGame}>Play Next Game</Button>
+
       <SeasonStanding seasonId={props.seasonId} />
       <SeasonGameList seasonId={props.seasonId} />
       <SeasonStandingSortable seasonId={props.seasonId} />
