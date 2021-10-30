dd531e46dcf48473aed62554e816ad0ece3393a7
David Rawlings <davidrawlings10@gmail.com>
2021-10-29 21:20:20 -0700

    fix: change TeamDisplay to function component

---
+++ src/Shared/TeamDisplay/TeamDisplay.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { searchCacheForTeam, cacheTeam } from "./TeamDisplayCache";
+import Team from "../../Entity/Team";
+
+export interface TeamDisplayFuncProps {
+  id: number;
+  hideLocation?: boolean;
+}
+
+export default function TeamDisplayFunc(props: TeamDisplayFuncProps) {
+  const [team, setTeam] = React.useState<Team>({
+    id: -1,
+    location: "",
+    name: "",
+  });
+
+  async function getTeamDisplay(id: number) {
+    var res = await fetch(
+      "http://localhost:8080/team/getTeamById?teamId=" + id
+    );
+    var team = await res.json();
+    return team;
+  }
+
+  React.useEffect(() => {
+    let team = searchCacheForTeam(props.id);
+
+    if (!!team) {
+      setTeam({ id: props.id, location: team.location, name: team.name });
+    } else {
+      getTeamDisplay(props.id).then((team) => {
+        setTeam({ id: props.id, location: team.location, name: team.name });
+        cacheTeam(props.id, team);
+      });
+    }
+  }, [props.id]);
+
+  return (
+    <span>
+      {props.hideLocation ? "" : team.location} {team.name}
+    </span>
+  );
+}
