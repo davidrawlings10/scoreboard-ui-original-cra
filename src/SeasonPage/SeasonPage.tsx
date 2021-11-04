51e5b3431cc0a3d2b93e1108a7a6214674b198cf
David Rawlings <davidrawlings10@gmail.com>
2021-11-04 14:36:34 -0700

    feat: display season header with all season details

--- src/SeasonPage/SeasonPage.tsx
+++ src/SeasonPage/SeasonPage.tsx
@@ -4,6 +4,7 @@ import { Box, Select, InputLabel, MenuItem } from "@material-ui/core";
 import SeasonList from "./SeasonList";
 import SeasonDisplay from "./SeasonDisplay";
 import Season from "../Entity/Season";
+import TeamDisplay from "../Shared/TeamDisplay/TeamDisplay";
 
 /*const useStyles = makeStyles({
   root: {
@@ -14,7 +15,7 @@ import Season from "../Entity/Season";
 export default function SeasonPage() {
   const [seasonId, setSeasonId] = useState(1);
   const [seasons, setSeasons] = useState(Array<Season>());
-  const [season, setSeason] = useState({ title: "" });
+  const [season, setSeason] = useState<any>({});
 
   function handleSeasonChange(event: React.ChangeEvent<any>) {
     if ((event.target.name as string) === "seasonId") {
@@ -39,6 +40,7 @@ export default function SeasonPage() {
     fetch("http://localhost:8080/season/findById?seasonId=" + seasonId)
       .then((res) => res.json())
       .then((json) => {
+        console.log(json);
         setSeason(json);
       });
   }, [seasonId]);
@@ -48,7 +50,6 @@ export default function SeasonPage() {
   return (
     <Box height="100%">
       <SeasonList viewSeason={viewSeason} />
-      <Box>{season.title}</Box>
       <InputLabel id="labelSeason" /*className={classes.root}*/>
         Season
       </InputLabel>
@@ -66,6 +67,59 @@ export default function SeasonPage() {
           </MenuItem>
         ))}
       </Select>
+      <Box marginTop={2} marginBottom={2} padding={1} border="1px solid black">
+        <Box padding={1} display="flex">
+          <Box margin={1}>
+            <h1>{season.title}</h1>
+          </Box>
+          <Box display="flex" flexDirection="column">
+            <Box
+              margin={1}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <Box>Winner</Box>
+              <TeamDisplay id={season.winnerTeamId} />
+            </Box>
+            <Box
+              margin={1}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <Box>Number of Teams</Box>
+              <Box>{season.numTeams}</Box>
+            </Box>
+            <Box
+              margin={1}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <Box>Schedule</Box>
+              {season.scheduleType}
+            </Box>
+            <Box
+              margin={1}
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+            >
+              <Box>League Id</Box>
+              <Box>{season.leagueId}</Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          border="1px solid black"
+          marginTop={1}
+          marginBottom={2}
+          padding={1}
+        >
+          {season.summary}
+        </Box>
+      </Box>
       <SeasonDisplay seasonId={seasonId} />
     </Box>
   );
