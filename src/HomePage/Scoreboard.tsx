99c5353cf0130ccfeb77470d6c72f665d3e3b28a
David Rawlings <davidrawlings10@gmail.com>
2021-11-11 18:59:29 -0800

    feat: add confirmation dialog to terminate game process

--- src/HomePage/Scoreboard.tsx
+++ src/HomePage/Scoreboard.tsx
@@ -1,10 +1,12 @@
-import TeamDisplay from "../Shared/TeamDisplay/TeamDisplay";
-import ClockDisplay from "../Shared/ClockDisplay";
-import Game from "../Entity/Game";
-
+import React from "react";
 import { Box, Grid, IconButton } from "@material-ui/core";
 import { Delete as DeleteIcon, Edit as EditIcon } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+
+import TeamDisplay from "../Shared/TeamDisplay/TeamDisplay";
+import ClockDisplay from "../Shared/ClockDisplay";
+import Game from "../Entity/Game";
+import ConfirmationDialog from "./ConfirmationDialog";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,6 +25,9 @@ export type ScoreboardProps = {
 export default function Scoreboard(props: ScoreboardProps) {
   const classes = useStyles();
 
+  const [confirmationDailogOpen, setConfirmationDailogOpen] =
+    React.useState(false);
+
   function adjustHandleClick() {
     if (!!props.game) {
       fetch(
@@ -32,81 +37,99 @@ export default function Scoreboard(props: ScoreboardProps) {
   }
 
   function terminateHandleClick() {
-    console.log("terminateHandleClick()");
-    if (!!props.game) {
-      fetch(
-        "http://localhost:8080/game/terminateCurrentGame?gameId=" +
-          props.game.id
-      );
-    }
+    setConfirmationDailogOpen(true);
   }
+
+  /*function deleteSomething() {
+    setConfirmationDailogOpen(true);
+    console.log("deleteSomething()");
+  }*/
+
+  const confirmationDialogHandleClose = (confirm: Boolean) => {
+    setConfirmationDailogOpen(false);
+
+    if (confirm) {
+      if (!!props.game) {
+        fetch(
+          "http://localhost:8080/game/terminateCurrentGame?gameId=" +
+            props.game.id
+        );
+      }
+    }
+  };
 
   if (!props.game) {
     return <div></div>;
   }
 
   return (
-    <Box display="flex" justifyContent="center">
-      <Box flexDirection="column">
-        <Box
-          bgcolor="primary.main"
-          border="1px solid black"
-          className={props.small ? classes.root : ""}
-          width={props.small ? 200 : 360}
-        >
-          <Grid container>
-            <Grid item xs={props.small ? 9 : 10}>
-              <Box border="1px solid black" p={1}>
-                <TeamDisplay
-                  id={props.game.homeTeamId}
-                  hideLocation={props.small}
-                />
-              </Box>
-            </Grid>
-            <Grid item xs={props.small ? 3 : 2}>
-              <Box border="1px solid black" p={1} textAlign="center">
-                {props.game.homeScore}
-              </Box>
-            </Grid>
-            <Grid item xs={props.small ? 9 : 10}>
-              <Box border="1px solid black" p={1}>
-                <TeamDisplay
-                  id={props.game.awayTeamId}
-                  hideLocation={props.small}
-                />
-              </Box>
-            </Grid>
-            <Grid item xs={props.small ? 3 : 2}>
-              <Box border="1px solid black" p={1} textAlign="center">
-                {props.game.awayScore}
-              </Box>
-            </Grid>
-            <Grid item xs={12}>
-              <Box border="1px solid black" p={1}>
-                <Box
-                  display="flex"
-                  flexDirection="row"
-                  justifyContent="space-between"
-                >
-                  <ClockDisplay game={props.game} />
+    <>
+      <Box display="flex" justifyContent="center">
+        <Box flexDirection="column">
+          <Box
+            bgcolor="primary.main"
+            border="1px solid black"
+            className={props.small ? classes.root : ""}
+            width={props.small ? 200 : 360}
+          >
+            <Grid container>
+              <Grid item xs={props.small ? 9 : 10}>
+                <Box border="1px solid black" p={1}>
+                  <TeamDisplay
+                    id={props.game.homeTeamId}
+                    hideLocation={props.small}
+                  />
                 </Box>
-              </Box>
+              </Grid>
+              <Grid item xs={props.small ? 3 : 2}>
+                <Box border="1px solid black" p={1} textAlign="center">
+                  {props.game.homeScore}
+                </Box>
+              </Grid>
+              <Grid item xs={props.small ? 9 : 10}>
+                <Box border="1px solid black" p={1}>
+                  <TeamDisplay
+                    id={props.game.awayTeamId}
+                    hideLocation={props.small}
+                  />
+                </Box>
+              </Grid>
+              <Grid item xs={props.small ? 3 : 2}>
+                <Box border="1px solid black" p={1} textAlign="center">
+                  {props.game.awayScore}
+                </Box>
+              </Grid>
+              <Grid item xs={12}>
+                <Box border="1px solid black" p={1}>
+                  <Box
+                    display="flex"
+                    flexDirection="row"
+                    justifyContent="space-between"
+                  >
+                    <ClockDisplay game={props.game} />
+                  </Box>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-        <Box display={props.small ? "none" : "flex"}>
-          <Box margin={0.5}>
-            <IconButton size="small" onClick={adjustHandleClick}>
-              <EditIcon />
-            </IconButton>
           </Box>
-          <Box margin={0.5}>
-            <IconButton size="small" onClick={terminateHandleClick}>
-              <DeleteIcon />
-            </IconButton>
+          <Box display={props.small ? "none" : "flex"}>
+            <Box margin={0.5}>
+              <IconButton size="small" onClick={terminateHandleClick}>
+                <DeleteIcon />
+              </IconButton>
+            </Box>
+            <Box margin={0.5}>
+              <IconButton size="small" onClick={adjustHandleClick}>
+                <EditIcon />
+              </IconButton>
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+      <ConfirmationDialog
+        open={confirmationDailogOpen}
+        onClose={confirmationDialogHandleClose}
+      />
+    </>
   );
 }
