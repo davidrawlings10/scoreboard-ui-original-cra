1cb538222d4dca1dd5d16fe0092fba1626997ffc
David Rawlings <davidrawlings10@gmail.com>
2022-08-01 17:55:14 -0700

    add League interface

---
+++ src/Entity/League.ts
@@ -0,0 +1,4 @@
+export default interface League {
+  value: string;
+  title: string;
+}
--- src/ScheduleSeasonForm.tsx
+++ src/ScheduleSeasonForm.tsx
@@ -13,24 +13,48 @@ import React, { useState, useEffect } from "react";
 import config from "./config";
 import { getLeagueList } from "./Shared/LeagueHelper";
 import Team from "./Entity/Team";
+import League from "./Entity/League";
 
 export type ScheduleSeasonFormProps = {};
 
 export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
-  const [leagues, setLeagues] = useState<Array<object>>();
+  const [leagues, setLeagues] = useState<Array<League>>();
   const [title, setTitle] = useState<string>("<Unnamed Season>");
   const [scheduleType, setScheduleType] = useState<string>(
     "HOME_ROTATION_RANDOM"
   );
-  const [league, setLeague] = useState<string>("NHL");
+  const [league, setLeague] = useState<League | undefined>(undefined);
   const [numGames, setNumGames] = useState<number>(4);
   const [showNumGamesInput, setShowNumGamesInput] = useState<boolean>(true);
   const [possibleTeams, setPossibleTeams] = useState<Array<Team>>([]);
   const [selectedTeamIds, setSelectedTeamIds] = useState<Array<number>>([]);
 
+  // load list of leagues
   useEffect(() => {
-    getLeagueList().then((list) => setLeagues(list));
+    getLeagueList().then((list) => {
+      setLeagues(list);
+      setLeague(list.find((league: League) => league.value === "NHL").value);
+    });
   }, []);
+
+  useEffect(() => {
+    if (!!league) {
+      fetch(config.baseUrl + "/team/getTeams?league=" + league)
+        .then((res) => res.json())
+        .then((json) => {
+          setPossibleTeams(json.list);
+        });
+    }
+  }, [league]);
+
+  // hide number of games input unless schedule type is Rounds
+  useEffect(() => {
+    if (scheduleType === "ROUNDS") {
+      setShowNumGamesInput(true);
+    } else {
+      setShowNumGamesInput(false);
+    }
+  }, [scheduleType]);
 
   function titleChange(event: React.ChangeEvent<any>) {
     setTitle(event.target.value);
@@ -61,6 +85,9 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
   }
 
   function handleSubmit(event: React.ChangeEvent<any>) {
+    if (!league) {
+      return;
+    }
     fetch(
       config.baseUrl +
         "/season/schedule?scheduleType=" +
@@ -77,23 +104,6 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
     );
     event.preventDefault();
   }
-
-  useEffect(() => {
-    fetch(config.baseUrl + "/team/getTeams?league=" + league)
-      .then((res) => res.json())
-      .then((json) => {
-        setPossibleTeams(json.list);
-      });
-  }, [league]);
-
-  // hide number of games input unless schedule type is Rounds
-  useEffect(() => {
-    if (scheduleType === "ROUNDS") {
-      setShowNumGamesInput(true);
-    } else {
-      setShowNumGamesInput(false);
-    }
-  }, [scheduleType]);
 
   return (
     <Box display="flex" justifyContent="center" width="100%" margin={2}>
@@ -119,7 +129,11 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
               >
                 {leagues &&
                   leagues.map((league: any) => (
-                    <MenuItem id={league.value} value={league.value}>
+                    <MenuItem
+                      key={league.value}
+                      id={league.value}
+                      value={league.value}
+                    >
                       {league.title}
                     </MenuItem>
                   ))}
@@ -167,24 +181,25 @@ export default function ScheduleSeasonForm(props: ScheduleSeasonFormProps) {
               justifyContent="start"
               height={360}
             >
-              {possibleTeams.map((team) => (
-                <Box>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        checked={selectedTeamIds.indexOf(team.id) >= 0}
-                        onChange={selectedTeamIdsChange}
-                        value={team.id}
-                      />
-                    }
-                    label={
-                      team.location
-                        ? team.location + " " + team.name
-                        : team.name
-                    }
-                  />
-                </Box>
-              ))}
+              {possibleTeams &&
+                possibleTeams.map((team) => (
+                  <Box key={team.id}>
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          checked={selectedTeamIds.indexOf(team.id) >= 0}
+                          onChange={selectedTeamIdsChange}
+                          value={team.id}
+                        />
+                      }
+                      label={
+                        team.location
+                          ? team.location + " " + team.name
+                          : team.name
+                      }
+                    />
+                  </Box>
+                ))}
             </Box>
           </Box>
           <Box display="flex" justifyContent="end">
