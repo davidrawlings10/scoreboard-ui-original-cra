61295844a58ac3103e9e3e1118ecee281f3477f8
David Rawlings <davidrawlings10@gmail.com>
2023-12-28 20:22:14 -0600

    update baseUrl config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 const config = {
-  baseUrl: "http://192.168.1.71:8080" /*"http://localhost:8080"*/,
-  // baseUrl: "http://172.18.98.159:8080" /*"http://localhost:8080"*/,
+  baseUrl: "http://192.168.1.71:8080",
+  // baseUrl: "http://172.18.98.159:8080",
+  // baseUrl: "http://localhost:8080"
   timeZone: "America/Los_Angeles",
 };
 
